8e90b369f1b16fcb489ea6b7384711577979e95b
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-02-11 12:30:16 +0200

    create account class

---
+++ password.py
@@ -0,0 +1,15 @@
+class Account:
+    """
+    Class that generates new instances of account.
+    """
+
+    account_list = [] # Empty account list
+
+    def __init__(self,account_name,user_name,password):
+
+      # docstring removed for simplicity
+
+        self.account_name = account_name
+        self.user_name = user_name
+        self.phone_number = number
+        self.password = password
