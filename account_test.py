34ad447660a3ec2f33fb1f17d70d8d462f7b421b
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-02-11 17:56:03 +0200

    add test

--- account_test.py
+++ account_test.py
@@ -14,7 +14,7 @@ class TestAccount(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_account = Account("Github","omukankurunziza","rukundo11") # create account object
+        self.new_account = Account("Mukankurunziza","omukankurunziza","rukundo11") # create account object
 
 
     def test_init(self):
@@ -22,7 +22,7 @@ class TestAccount(unittest.TestCase):
         test_init test case to test if the object is initialized properly
         '''
 
-        self.assertEqual(self.new_account.account_name,"Github")
+        self.assertEqual(self.new_account.account_name,"Mukankurunziza")
         self.assertEqual(self.new_account.user_name,"omukankurunziza")
         self.assertEqual(self.new_account.password,"rukundo11")
 
@@ -42,7 +42,7 @@ class TestAccount(unittest.TestCase):
             objects to our account_list
             '''
             self.new_account.save_account()
-            test_account = Account("yahoo.fr","nshutioppo@yahoo.fr","rukundo") # new account
+            test_account = Account("Mukankurunziza","nshutioppo@yahoo.fr","rukundo") # new account
             test_account.save_account()
             self.assertEqual(len(Account.account_list),2)
 
@@ -60,7 +60,7 @@ class TestAccount(unittest.TestCase):
             objects to our account_list
             '''
             self.new_account.save_account()
-            test_account = Account("yahoo.fr","nshutioppo@yahoo.fr","rukundo") # new account
+            test_account = Account("Mukankurunziza","nshutioppo@yahoo.fr","rukundo") # new account
             test_account.save_account()
             self.assertEqual(len(Account.account_list),2)
     # More tests above
@@ -69,11 +69,67 @@ class TestAccount(unittest.TestCase):
             test_delete_account to test if we can remove a account from our account list
             '''
             self.new_account.save_account()
-            test_account = Account("yahoo.fr","nshutioppo@yahoo.fr","rukundo") # new contact
+            test_account = Account("Mukankurunziza","nshutioppo@yahoo.fr","rukundo") # new contact
             test_account.save_account()
 
             self.new_account.delete_account()# Deleting a account object
             self.assertEqual(len(Account.account_list),1)
+
+
+    def test_find_account_by_account_name(self):
+        '''
+        test to check if we can find a account by account_name  and display information
+        '''
+
+        self.new_account.save_account()
+        test_account = Account("Mukankurunziza","nshutioppo@yahoo.fr","rukundo") # new account
+        test_account.save_account()
+
+        found_account = Account.find_by_account_name("Mukankurunziza")
+
+        self.assertEqual(found_account.user_name,test_account.user_name)
+
+    @classmethod
+    def find_by_account_name(cls,account_name):
+        '''
+        Method that takes in a account_name  and returns a account that matches that account_name.
+
+        Args:
+            account_name: account_name to search for
+        Returns :
+            account of person that matches the account_name.
+        '''
+
+        for account in cls.account_list:
+            if account.account_name == account_name:
+                return account
+    
+    def test_account_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the account.
+        '''
+
+        self.new_account.save_account()
+        test_account = Account() # new contact
+        test_account.save_account("Mukankurunziza","nshutioppo@yahoo.fr","rukundo")
+
+        account_exists = Account.account_exist("Mukankurunziza")
+
+        self.assertTrue(account_exists)
+
+    @classmethod
+    def account_exist(cls,account_name):
+        '''
+        Method that checks if a account exists from the account list.
+        Args:
+            account_name: account_name to search for
+            Boolean: True or false depending if the account exists
+        '''
+        for account in cls.account_list:
+            if account.account_name == account_name:
+                    return True
+
+        return False
 
        
 
